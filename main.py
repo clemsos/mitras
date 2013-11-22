cbdc466489ac2abf17a76b584d021f49142592ee
clemsos <clement.renaud@gmail.com>
2013-11-22 16:10:35 +0100

    basic heatmap with d3.js

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import numpy as np
 from time import time
 import pylab
 from scipy import array as sparray
-from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.cluster.hierarchy import linkage, dendrogram,leaves_list
 from scipy.cluster.vq import kmeans,vq
 from scipy.spatial.distance import pdist, squareform
 
@@ -24,11 +24,19 @@ import matplotlib.pyplot as plt
 t00=time()
 
 count=60
-protomemes=get_protomemes(count)
+pms=get_protomemes("hashtags",count)
+# convert to numpy array 
+protomemes=np.array(pms)
 
 print "%d protomemes obtained." % len(protomemes)
 print 
 
+print
+print "Label matrix with protomemes description"
+
+labels=[p["value"]["type"] +" : "+p["_id"] for p in protomemes]
+
+print " length of labels %d"%len(labels)
 
 print '#'*40
 print "Step 1 : Compute all vectors from protomemes"
@@ -123,15 +131,68 @@ if wt+wc+wu+wd != 1:
 # TODO : add missing parameters
 print " weighting and scaling up matrix "
 combi=wc*np.array(text_sim) +wd*np.array(diffusion_sim) #+wt*np.array(tweets_sim)
+print " combination length :%d " % len(combi)
 
-print " computing clusters with average linkage algorithm"
-clusters=linkage(combi, method='average')
-print " clusters: n_samples: %d, n_features: %d" % clusters.shape
+print " calculate matrix w average linkage algorithm"
+linkage_matrix=linkage(combi, method='average')
+print " clusters: n_samples: %d, n_features: %d" % linkage_matrix.shape
+
+def explain_clusters(clusters):
+    for row in clusters:
+        i=int(row[0])
+        value1="cluster"
+        if i in range(0,len(protomemes)):
+            value1=labels[i]    
+
+        j=int(row[0])
+        value2="cluster"
+        if j in range(0,len(protomemes)):
+            value2=labels[j]
+
+        print row
+        print "---> [",value1,value2, row[2],"]"
 
 
-for row in clusters:
-    print row
+print " get order from dendrogram leaves"
+reordered = leaves_list(linkage_matrix)
 
+#reorder the data matrix and row headers according to leaves
+ordered_data_matrix = combi[reordered,:]
+
+#do the same for the row headers
+row_headers = np.array(labels)
+ordered_row_headers = row_headers[reordered,:]
+
+#output data for visualization in a browser with javascript/d3.js
+matrixOutput = []
+row = 0
+for rowData in ordered_data_matrix:
+    col = 0
+    rowOutput = []
+    for colData in rowData:
+        rowOutput.append([colData, row, col])
+        col += 1
+    matrixOutput.append(rowOutput)
+    row += 1
+
+
+# Export to js vars for visualization with d3.js
+# BUG : not working
+jsfile=""
+jsfile+='var maxData = ' + str( np.amax(protomemes) ) + ";"
+jsfile+='\n\n'
+jsfile+= 'var minData = ' + str(np.amin(protomemes)) + ";"
+jsfile+='\n\n'
+jsfile+= 'var data = ' + str(matrixOutput) + ";"
+jsfile+='\n\n'
+jsfile+= 'var cols = ' + str(labels) + ";"
+jsfile+='\n\n'
+jsfile+= 'var rows = ' + str([x for x in ordered_row_headers]) + ";"
+
+with open('ui/data/data.js', 'w') as myFile:
+    myFile.write(jsfile)
+
+print
 print " plotting data and generating images"
 # use vq() to get as assignment for each obs.
 # assignment,cdist = vq(clusters,clusters)
@@ -154,7 +215,7 @@ def augmented_dendrogram(*args, **kwargs):
                          va='top', ha='center')
 
 #     return ddata
-print clusters
+# print clusters
 
 # # ss=dendrogram(clusters)
 # show_leaf_counts = True
