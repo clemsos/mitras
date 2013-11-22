9241902873c4bd5166e4c705fe4f832604fc56a2
clemsos <clement.renaud@gmail.com>
2013-11-22 15:03:15 +0100

    utils to convert string to filename

---
+++ lib/utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def slugify(value):
+    import unicodedata
+    import re
+
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    value
+    if type(value) is str:
+        value=value.decode('utf-8');
+
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    value = unicode(re.sub('[-\s]+', '_', value))
+
+    return value
