0bd013e80319bb0e637c32044b21529c37e6ea75
clemsos <clement.renaud@gmail.com>
2013-12-02 16:20:32 +0100

    a meme gephi library

--- lib/memes.py
+++ lib/memes.py
@@ -4,6 +4,7 @@
 from time import time
 from lib.mongo import MongoDB
 from models.meme import Meme
+import csv
 
 # Connect to Mongo
 db=MongoDB("weibodata").db
@@ -57,3 +58,46 @@ def create_meme_from_protomemes(_name,_protomemes_ids):
     print
     print " done in %fs" % (time() - t0)
     # return dates,values
+
+def list_to_csv(_keys,_rows,_csv_filepath):
+    
+    # get keys for csv value
+    # keys=_rows[0].keys()
+
+    print " extracting %s from tweets"%(_keys)
+    # write to csv
+    with open(_csv_filepath,'w') as f: # writes the final output to CSV
+        csv_out=csv.writer(f)
+        csv_out.writerow(_keys) # add header
+        for row in _rows:
+            csv_out.writerow(row)
+
+    print " csv has been stored as %s"%_csv_filepath
+
+def meme_to_gephi_csv(_name,_dir_path):
+    t0=time()
+
+    # get meme data
+    query={ "name" : _name}
+    meme=list(db["memes"].find(query))[0]
+    print " tweets in meme :  %d" % len(meme["tweets"])
+
+    # 
+    nodes=[]
+    edges=[]
+
+    for i,t in enumerate(meme["tweets"]):
+        nodes.append( (t["userId"],"user"+str(i)) )
+
+        # add mentions
+        for m in t["mentions"]:
+            edges.append((t["userId"],t["userId"]))
+        
+        # add RTs
+        if t["retweetFromUserId"] != "": 
+            edges.append((t["retweetFromUserId"],t["userId"]))
+
+    list_to_csv(["Id", "Label"],nodes,_dir_path + '/'+_name+'_nodes.csv')
+    list_to_csv(["Source","Target"],edges,_dir_path +'/'+_name+'_edges.csv')
+
+    print " done in %fs" % (time() - t0)
