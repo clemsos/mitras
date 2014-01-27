8aaf02ca254a5b4d1a09b90f480f9cb8ca9b3c01
clemsos <clement.renaud@gmail.com>
2014-01-27 09:33:46 +0100

    remove setup

--- setup/install_blas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-workon [envname]
-pip uninstall numpy ## only if numpy is already installed
-pip uninstall scipy ## only if scipy is already installed
-export LAPACK=/usr/lib/liblapack.so
-export ATLAS=/usr/lib/libatlas.so
-export BLAS=/usr/lib/libblas.so
